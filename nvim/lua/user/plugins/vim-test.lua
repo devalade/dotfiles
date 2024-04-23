27ef17a372a99e913d5eeb6387030b07d381c90e
Alade Yessoufou <aladecharaf23@gmail.com>
2024-04-23 15:22:56 +0100

    first commit

---
+++ nvim/lua/user/plugins/vim-test.lua
@@ -0,0 +1,26 @@
+return {
+  'vim-test/vim-test',
+  keys = {
+    { '<Leader>TN', ':TestNearest<CR>' },
+    { '<Leader>TF', ':TestFile<CR>' },
+    { '<Leader>TS', ':TestSuite<CR>' },
+    { '<Leader>TL', ':TestLast<CR>' },
+    { '<Leader>TV', ':TestVisit<CR>' },
+  },
+  dependencies = { 'voldikss/vim-floaterm' },
+  config = function()
+    vim.cmd([[
+      let test#php#phpunit#options = '--colors=always'
+      let test#php#pest#options = '--colors=always'
+
+      function! FloatermStrategy(cmd)
+        execute 'silent FloatermSend q'
+        execute 'silent FloatermKill'
+        execute 'FloatermNew! '.a:cmd.' |less -X'
+      endfunction
+
+      let g:test#custom_strategies = {'floaterm': function('FloatermStrategy')}
+      let g:test#strategy = 'floaterm'
+    ]])
+  end
+}
